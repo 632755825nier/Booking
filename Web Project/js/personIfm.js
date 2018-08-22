bb16e636e25a3cceed12723fe1e26dc78d003ffb
nier <17875512076@163.com>
2018-08-22 20:27:48 +0800

    personIfm

--- Web Project/js/personIfm.js
+++ Web Project/js/personIfm.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
     $('.order_ifm_p1').mousemove(function (e) {
         var top = e.pageY + 15;
-        var left = e.pageX + 20; //使得文字块显示在鼠标的右下方
+        var left = e.pageX -31; //使得文字块显示在鼠标的右下方
         $('.tips').css({
             'top': top + 'px',
             'left': left + 'px'
@@ -22,7 +22,8 @@ $(document).ready(function () {
 
 
     $('.order_information_next1_p').hover(function () {
-        $('.tip3').slideDown();
+        //$('.tip3').show();
+        $('.tip3').show("show");
         $('.tip3').text($('.order_information_next1_p').attr('data-title'));
     });
 
@@ -35,17 +36,112 @@ $(document).ready(function () {
         });
     });
 
+
+    //显示图片
+    $('.container_order_ifm_img').hover(function () {
+        $('.modal').show();
+    });
+
+    $('.order_ifm_r_img1').hover(function () {
+        $('.modal2').show();
+    });
+    $('.order_ifm_r_img2').hover(function () {
+        $('.modal3').show();
+    });
+    $('.order_ifm_r_img3').hover(function () {
+        $('.modal4').show();
+    });
+    $('.order_ifm_r_img4').hover(function () {
+        $('.modal5').show();
+    });
+
+    //图片移动
+    $('.container_order_ifm_img').mousemove(function (e) {
+        var top = e.pageY + 15;
+        var left = e.pageX + 20; //使得文字块显示在鼠标的右下方
+        $('.modal').css({
+            'top': top + 'px',
+            'left': left + 'px'
+        });
+    });
+
+    $('.order_ifm_r_img1').mousemove(function (e) {
+        var top = e.pageY + 15;
+        var left = e.pageX + 20; //使得文字块显示在鼠标的右下方
+        $('.modal2').css({
+            'top': top + 'px',
+            'left': left + 'px'
+        });
+    });
+
+    $('.order_ifm_r_img2').mousemove(function (e) {
+        var top = e.pageY + 15;
+        var left = e.pageX + 20; //使得文字块显示在鼠标的右下方
+        $('.modal3').css({
+            'top': top + 'px',
+            'left': left + 'px'
+        });
+    });
+
+    $('.order_ifm_r_img3').mousemove(function (e) {
+        var top = e.pageY + 15;
+        var left = e.pageX + 20; //使得文字块显示在鼠标的右下方
+        $('.modal4').css({
+            'top': top + 'px',
+            'left': left + 'px'
+        });
+    });
+
+    $('.order_ifm_r_img4').mousemove(function (e) {
+        var top = e.pageY + 15;
+        var left = e.pageX + 20; //使得文字块显示在鼠标的右下方
+        $('.modal5').css({
+            'top': top + 'px',
+            'left': left + 'px'
+        });
+    });
+
+    //图片隐藏
+    $('.container_order_ifm_img').mouseout(function () {
+        $('.modal').hide();
+    });
+
+    $('.order_ifm_r_img1').mouseout(function () {
+        $('.modal2').hide();
+    });
+
+    $('.order_ifm_r_img2').mouseout(function () {
+        $('.modal3').hide();
+    });
+    $('.order_ifm_r_img3').mouseout(function () {
+        $('.modal4').hide();
+    });
+    $('.order_ifm_r_img4').mouseout(function () {
+        $('.modal5').hide();
+    });
+
+
+
+
+
     $('.order_information_next1_p').mouseout(function () {
         $('.tip3').hide();
     });
 
-    //$(".input_ifm_centre_inp").focusout(function(){
-    //   //alert("表单事件！")
-    //   // if($(".input_ifm_centre_inp").value()==null){
-    //   //     alert("没有内容");
-    //   // }else{
-    //        alert("有内容"+$(".input_ifm_centre_inp").value());
-    //
-    //    }
-    //});
+
+
+    //表单提示
+    $(".input_ifm_centre_inp").click(function(){
+        //alert("键盘输入！") ;
+        var msg = $(".input_ifm_centre_inp").val();
+        $(".input_ifm").click(function(){
+            if(msg==null){
+                alert("表单没有内容");
+            }else{
+                alert(msg);
+            }
+        });
+
+    });
+
 });
